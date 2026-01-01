9dcbab252fa6947356a60d80be033d77c4d0d7ff
lucky <crackerlucky0@gmail.com>
2026-01-01 14:16:57 +0530

    day 4 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/pages/home_page.dart';
 import 'package:flutter_application_1/pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,14 +16,12 @@ class MyApp extends StatelessWidget {
     //build is very imp yha ui ka kaam hota hai
     return MaterialApp(
       themeMode: ThemeMode.light,
-      // theme: ThemeData(
-        // primarySwatch: Colors.deepPurple),
+      theme: ThemeData(
+      primarySwatch: Colors.deepPurple,
+      fontFamily: GoogleFonts.lato().fontFamily),
       darkTheme: ThemeData(brightness: Brightness.dark),
-      initialRoute: "/home",
-      routes: {
-        "/":(context) => LoginPage(),
-        "/home":(context) => HomePage(),
-      },
-      );
+      // initialRoute: "/",
+      routes: {"/": (context) => LoginPage(), "/home": (context) => HomePage()},
+    );
   }
 }
